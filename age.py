182e5a38ececcd11ad68e9f7ec2d7234e39431d5
joops680 <joshuasalie85@outlook.com>
2024-05-16 10:11:56 +0200

    added comments

--- age.py
+++ age.py
@@ -1,5 +1,7 @@
+# asking user there age
 age = int(input("What is your age? "))
 if age < 18:
+    # checking if your older enough or a minor
     print("You are a minor.")
 else:
     print("You just became an adult.")
